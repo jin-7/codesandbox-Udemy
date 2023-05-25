1fd541f9f5300e4bae0d293db7a663c84210cd8b
ゴン山 <102216157+jin-7@users.noreply.github.com>
2023-05-25 13:45:25 +0000

    Update 6 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import "./styles.css";
+const name = "じゃえぇ";
+const age = 28;
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(message1);
+
+const message2 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message2);
